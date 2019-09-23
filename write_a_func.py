740304bec49dcd85faee540979f508a3a99111cd
aboyar <54912300+aboyar@users.noreply.github.com>
2019-09-23 14:26:27 -0500

    Add files via upload

---
+++ write_a_func.py
@@ -0,0 +1,14 @@
+def is_leap(year):
+    leap = False
+    if year % 4 == 0: 
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            return False
+        return True
+    else:
+        return False
+
+    return leap
+year = int(input())
+print(is_leap(year))
